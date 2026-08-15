2417ba1c3f2ac6c512d1e82f3dc0a93638cd55d8
lorddarknessytr-wq <lord.darknessytr@gmail.com>
2026-08-15 17:55:17 +0330

    Update all_mod-v0.js

--- all_mod-v0.js
+++ all_mod-v0.js
@@ -72,7 +72,7 @@ let db = [
         downloads: 1250,
         likes: 34,
         tags: ['#1'],
-        downloadLink: 'https://imaniweb.ir/?download=088553'
+        downloadLink: 'https://imaniweb.ir/?download=816098'
     },
     {
         id: 2,
