fd17cca70dd278d740f3ad5564a2bdcc0d074931
lorddarknessytr-wq <lord.darknessytr@gmail.com>
2026-08-17 04:33:33 +0330

    Update all_mod-v0.js

--- all_mod-v0.js
+++ all_mod-v0.js
@@ -234,13 +234,13 @@ let db = [
     {
         id: 1,
         type: 'ADD-ON',
-        title: '',
-        desc: 'توضیحات',
-        img: '',
+        title: 'مود RGB',
+        desc: `افزونه RPG، بیش از 50 عنصر RPG را به هر دنیایی اضافه می‌کند: کلاس‌ها را انتخاب کنید، سطح خود را ارتقا دهید، انجمن‌ها بسازید و موارد دیگر! مهارت‌های جدید، نبردهای چالش‌برانگیز با باس‌ها، زیست‌بوم‌های کمیاب و صندوق‌های گنج، موجودات منحصربه‌فرد، قبایل، همراهان، و همچنین مجموعه‌ای از سلاح‌ها و زره‌ها. `,
+        img: 'https://abrehamrahi.ir/o/public/uWdJUXpQ/',
         downloads: 0,
         likes 0,
         tags: ['#'],
-        downloadLink: ''
+        downloadLink: 'https://abrehamrahi.ir/o/public/YX728ok9/'
     },
     {
         id: 2,
